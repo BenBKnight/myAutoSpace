c9527b63391152c4e035f8d53197c91428171c46
Ben K <ben.knight.brazil@hotmail.com>
2020-07-23 18:54:11 -0600

    added server file information

--- server.js
+++ server.js
@@ -1,10 +1,36 @@
 const express = require("express");
+const session = require("express-session");
 const path = require("path")
 const app = express();
 const PORT = 8080;
+const mysql = require("mysql");
+const passport = require("./config/passport");
+
 
 // Serve static assets
 app.use(express.static("./client"));
+
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "password",
+        database: "carFacts"
+    });
+}
+// Creating express app and configuring middleware needed for authentication
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// We need to use sessions to keep track of our user's login status
+app.use(
+    session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 // API routes are loaded before "catch all"
 const api = ["api info", "api info"]
