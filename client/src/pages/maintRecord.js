7ca97c4146627bcb49195f4342e7f04bf79f59e5
Ben K <ben.knight.brazil@hotmail.com>
2020-07-29 00:57:55 -0600

    deleted old code

--- client/src/pages/maintRecord.js
+++ client/src/pages/maintRecord.js
@@ -37,26 +37,6 @@ class MaintRecord extends Component {
             })
     };
 
-    // const id = window.location.href.split("/");
-
-    // // id[id.length - 1];
-
-    // $(document).ready(() => {
-    //     $.ajax({
-    //         url: `/maintenancefind/${id[id.length - 1]}`,
-    //         type: "GET"
-    //     })
-    //         .then(result => {
-    //             // console.log(result[0].name);
-    //             $("#jobName").append(result[0].name);
-    //             $("#jobDate").append(`(${result[0].jobDate})`);
-    //             $("#milage").append(`${result[0].milage}`);
-    //             $("#description").append(`${result[0].description}`);
-    //             $("#parts").append(`${result[0].parts}`);
-    //         })
-    //         .catch(err => console.log(err));
-    // });
-
     render() {
         return (
             <div>
