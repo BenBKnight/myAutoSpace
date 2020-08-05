9f457356b60e2ace235f86294e7c7cb1f48d0d31
george-g <georgegombert@gmail.com>
2020-08-05 14:45:21 -0400

    Deleted unused items

--- client/src/StyledPages/components/Unused Components/LogInBtn.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function LogInBtn(props) {
-  return(
-    <button onClick = {props.onClick}><h1>Log In</h1></button>
-  );
-}
-
-export default LogInBtn;
--- client/src/StyledPages/components/Unused Components/LogOutBtn.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function LogOutBtn(props) {
-  return(
-    <button onClick = {props.onClick}><h1>Log Out</h1></button>
-  );
-}
-
-export default LogOutBtn;
